4267e5c768896133b344e6daeef7268677fdc1ba
tuq33158 <tuq33158@temple.edu>
2024-08-30 01:47:35 -0400

    tested

--- print_random.c
+++ print_random.c
@@ -1,4 +1,18 @@
 //Given Project code
+#include <stdlib.h>
+#include <stdio.h>
+#include "random.h"
+#include <time.h> 
+
+void rand_string(char *s, size_t size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        s[i] = randchar();
+        s[size] = '\0';
+    }
+}
+
 char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
@@ -6,4 +20,13 @@ char* rand_string_alloc(size_t size)
          rand_string(s, size);
      }
      return s;
+}
+
+int main()
+{
+    srand(time(0)); //Starting random num generator
+    char* randomWord = rand_string_alloc(7); //Create 7 letter word
+    printf("Test String is %s\n", randomWord);
+    free(randomWord);
+    return 0;
 }
